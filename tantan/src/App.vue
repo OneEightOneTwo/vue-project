d56b7f0acf62c0c881be6d1f0930c8df7b855a93
masturiri <2720601448@qq.com>
2019-04-22 09:09:14 +0800

    完成axios跨域，API请求

--- tantan/src/App.vue
+++ tantan/src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <TTTText />
   </div>
 </template>
 
 <script>
+import TTTText from "./components/TTTText.vue";
 // import TTheader from "./components/Home/TTheader.vue";
 // // import TTmain from './components/Home/TTmain.vue'
 // import TTmain3 from "./components/Home/TTmain3.vue";
 // // import RightReveal from "./pages/RightReveal.vue";
-// export default {
-//   name: "app",
-//   components: {
-//     TTheader,
-//     // TTmain
-//     TTmain3,
-//     // RightReveal
-//   }
-// };
+export default {
+  name: "app",
+  components: {
+    TTTText
+    // TTheader,
+    // TTmain
+    // TTmain3,
+    // RightReveal
+  }
+};
 </script>
 
 <style>
---
+++ tantan/src/components/TTTText.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <div class="wrap">
+      <transition name="show">
+        <div
+          class="d"
+          v-for="(item, index) in list"
+          :key="index"
+        ><p v-if="count === index">{{ item.text }}</p></div>
+      </transition>
+    </div>
+    <button @click="add">add</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue.js!",
+      count: 0,
+      list: [
+        { id: 0, text: "aaa" },
+        { id: 1, text: "bbb" },
+        { id: 2, text: "ccc" }
+      ]
+    };
+  },
+  methods: {
+    add: function() {
+      if (this.count < this.list.length - 1) {
+        this.count += 1;
+      } else {
+        this.count = 0;
+      }
+    }
+  }
+};
+</script>
+<style>
+  .d {
+            position: absolute;
+            border: 1px solid red;
+            width: 30px;
+            height: 30px;
+        }
+        @keyframes show {
+            0% {
+                opacity: 0;
+                left: 32px;
+            }
+            100% {
+                opacity: 1;
+                left: 0;
+            }
+        }
+        @keyframes hide {
+            0% {
+                opacity: 1;
+                left: 0;
+            }
+            100% {
+                opacity: 0;
+                left: -32px;
+            }
+        }
+        .show-enter-active {
+            animation: show 1.2s;
+        }
+        .show-leave-active {
+            animation: hide 1.2s;
+        }
+        .show-enter, .show-leave-to {
+            opacity: 0;
+        }
+        .wrap {
+            position: relative;
+            width: 32px;
+            height: 32px;
+        }
+
+</style>
+
